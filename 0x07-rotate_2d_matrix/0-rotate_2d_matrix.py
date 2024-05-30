a3657737371c3d8152432952e079c93b7b8ce58a
Matrix30 <Matrixboy30@gmail.com>
2024-05-30 22:57:47 +0100

    finish task 0. Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+contains rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotates an n x n 2d matrix by 90 degrees
+    clockwise
+                Args:
+                        matrix(List(List)): matrix to rotate
+                Returns:
+                        None;
+    """
+    # matrix = [[matrix[i][j] for i in range(len(matrix))]
+    # for j in range(len(matrix[0]))]
+    size = len(matrix)
+    for i in range(size):
+        for j in range(i, size):
+            matrix[i][j], matrix[i][j] = matrix[j][i], matrix[j][i]
+    for m in matrix:
+        m.reverse()
