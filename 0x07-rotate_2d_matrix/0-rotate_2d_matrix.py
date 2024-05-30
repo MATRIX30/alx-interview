af8af6432eaeabf72d1e0a4d32879220e130feb6
Matrix30 <Matrixboy30@gmail.com>
2024-05-30 23:43:19 +0100

    fix matrix indexing

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,7 +18,7 @@ def rotate_2d_matrix(matrix):
     size = len(matrix)
     for i in range(size):
         for j in range(i, size):
-            matrix[i][j], matrix[i][j] = matrix[j][i], matrix[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     for m in matrix:
         m.reverse()
     return
